55808301ae99520b9242ddd960496a07e7d83a92
janvizinzuvadiya <zinzuvadiyajanvi4@gmail.com>
2026-08-11 13:21:43 +0530

    Mongoose Connection with Node js

--- mongoose_crud/crudapp/index.js
+++ mongoose_crud/crudapp/index.js
@@ -1,28 +1,110 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dns from 'node:dns';
+import { type } from 'node:os';
 
-const mongo_url = "mongodb+srv://zinzuvadiyajanvi4_db_user:LvzuH9MpcMxbqQj0@cluster0.s3l6uab.mongodb.net/node_db"
+dns.setServers(["8.8.8.8","1.1.1.1"]);
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 
-app.get('/Books', (req, res) => {
-    res.json({ message: 'Getting All Books' });
+const mongo_url = "mongodb+srv://zinzuvadiyajanvi4_db_user:LvzuH9MpcMxbqQj0@cluster0.s3l6uab.mongodb.net/node_db"
+
+if(!mongo_url)
+{
+    console.error("Please provide MongoDB URL")
+    process.exit(1);
+}
+
+mongoose.connect(mongo_url).then(() => {
+    console.log("MongoDB connected successfully");
+}).catch((error) => {
+    console.error("MongoDB connection failed", error);
+    process.exit(1);
 });
 
-app.post('/Books', (req, res) => {
-    res.json({ message: 'Creating A Book', data: req.body });
+const BooksSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    category:{
+        type: String,
+        required : true
+    }
+}); 
+
+const Book = mongoose.model("Books",BooksSchema);
+
+app.get('/Books', async(req, res) => {
+    try
+    {
+        const obj = await Book.find();
+        res.status(200).json({ message: "Books fetched successfully", data: obj });
+    }
+    catch(error)
+    {
+        res.status(500).json({ message: "Error fetching books", error });
+    }
+});
+ 
+app.post('/Books', async (req, res) => {
+    
+    try
+    {
+        // const obj = new Book(req.body);
+        // await obj.save();
+
+        const obj = await Book.create(req.body);
+
+        res.status(201).json({ message: "Book created successfully", data: obj });
+    }
+    catch(error)
+    {
+        res.status(500).json({ message: "Error creating book", error });
+    }
+
 });
 
-app.put('/Books/:id', (req, res) => {
-    res.json({ message: 'Updating A Book', id: req.params.id, data: req.body });
+app.put('/Books/:id',async (req, res) => {
+   
+    try
+    {
+        const obj = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!obj) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.status(200).json({ message: "Book updated successfully", data: obj });
+    }
+    catch(error)
+    {
+        res.status(500).json({ message: "Error updating book", error });
+    }
 });
 
-app.delete('/Books/:id', (req, res) => {
-    res.json({ message: 'Deleting A Book', id: req.params.id });
+app.delete('/Books/:id',async (req, res) => {
+    try
+    {
+        const obj = await Book.findByIdAndDelete(req.params.id);
+
+        if (!obj) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.status(200).json({ message: "Book deleted successfully",id:req.params.id });
+    }
+    catch(error)
+    {
+        res.status(500).json({ message: "Error deleting book", error });
+    }
 });
 
 
